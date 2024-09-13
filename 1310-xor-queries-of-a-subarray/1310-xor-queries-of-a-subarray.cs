222d76191afa3ecca3e7fe97b44cb713c6190870
garcia-sebastianf <76495580+garcia-sebastianf@users.noreply.github.com>
2024-09-13 10:04:51 -0500

    Time: 828 ms (9.09%), Space: 67.9 MB (90.91%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cs
@@ -0,0 +1,44 @@
+//Created by: Sebastián Felipe García Rojas
+
+public class Solution {
+    public int[] XorQueries(int[] arr, int[][] queries) {
+        
+        int finish, init = 0;
+        int[] output  = new int[queries.Length];
+        int xoroperation = 0;
+        int i = 0;
+        bool once = false;
+        
+        foreach (int[] querie in queries){
+            
+            init = querie[0];
+            finish = querie[1];
+            once = false;
+            
+            if (init == finish) {
+                    xoroperation = arr[init];
+            }else{
+                
+                for (int k = init; k <= finish; k++){
+                
+                
+                    if (k < finish){
+                        if (once == false){
+                            xoroperation = arr[k] ^ arr[k+1];
+                            once = true;
+                            continue;
+                        }
+                    
+                        xoroperation = xoroperation ^ arr[k+1];
+                    
+                    } 
+                }
+            }
+            
+            output[i] = xoroperation;
+            i++;
+        }
+        
+        return output;
+    }
+}
