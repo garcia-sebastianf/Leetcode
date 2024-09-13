2a9a60a85a1d8e02a2ad5215fce9c6ee8fbadf7b
garcia-sebastianf <76495580+garcia-sebastianf@users.noreply.github.com>
2024-09-12 21:02:08 -0500

    Time: 112 ms (46.70%), Space: 50.8 MB (90.11%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cs
@@ -0,0 +1,33 @@
+//Created by: Sebastián Felipe García Rojas
+
+/**/
+
+public class Solution {
+    public int CountConsistentStrings(string allowed, string[] words) {
+        
+        bool LetterIsConsisted = false;
+        bool WordIsConsisted = true;
+        int consistedWords = 0;
+            
+        foreach (string word in words){
+            foreach (char letterWord in word){
+                foreach (char letterStringAllowed in allowed){
+                    if (letterStringAllowed == letterWord){
+                        LetterIsConsisted = true;
+                    }
+                }
+                if (LetterIsConsisted == false){       
+                    WordIsConsisted = false;
+                    break;
+                }
+                LetterIsConsisted = false;
+            }
+            if (WordIsConsisted == true){
+                consistedWords++;
+                LetterIsConsisted = false;
+            }
+            WordIsConsisted = true; 
+        }
+        return consistedWords;      
+    }
+}
